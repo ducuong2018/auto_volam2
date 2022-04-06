94abe02538ab39d814987da13ea24d3e02859864
CuongDC <ducuong2017@gmail.com>
2022-04-06 22:36:13 +0700

    Add project files.

---
+++ auto/Program.cs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sales
+{
+    static class Program
+    {
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+        Process p = new Process();
+        p.StartInfo.FileName = "cmd.exe";
+        p.StartInfo.Arguments = @"/C ping www.google.com";
+        p.StartInfo.RedirectStandardOutput = true;
+        p.StartInfo.UseShellExecute = false;
+        p.Start();
+        string output = p.StandardOutput.ReadToEnd();
+        p.WaitForExit();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+}
